43e97a28297b4d5d6f12ecbfa536493feb0b286e
sanaaDaire <sanaa.daire@icloud.com>
2020-03-16 13:48:00 +0200

    q3

---
+++ js/q1.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var numbers= [2,3,4];
+
+function myNumbers (){
+
+    var greatest = Math.max [numbers];
+}
+
+myNumbers ();
+// console.log(numbers);
---
+++ js/q2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function stars(){
+
+var text = " * " ;
+
+for (var i ; i<6 ; i++){
+    
+    if (i == 3 ){
+        continue;
+    }
+
+    text += i ;
+}
+
+
+}
+stars();
+console.log( stars);
